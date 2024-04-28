d13efb64fc4a5acb16a28f8640ad61c9fc142724
João Pedro <jpedrocruz78@gmail.com>
2024-04-28 18:23:26 -0400

    finish implementing main logic

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function getHumanChoice()
     let choice;
     let playerInput;
     playerInput = prompt("What do you choose? Rock, paper or scissors? ");
-    playerInput = playerInput.toLowerCase;
+    playerInput = playerInput.toLowerCase();
     switch (playerInput)
     {
         case "rock":
@@ -49,46 +49,71 @@ function waitTwoSeconds()
     return new Promise(resolve => setTimeout(resolve, 2000));
 }
 
- function playGame()
- {
-    let humanScore = 0;
-    let computerScore = 0;
+/**
+ * Main function, that initiantes the game
+ */
+function playGame()
+{
+let humanScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
 
-    function playRound(computerChoice, humanChoice)
+/**
+ * Function that plays a round of the game
+ * @param {*} computerChoice 
+ * @param {*} humanChoice 
+ * @returns 
+ */
+function playRound(computerChoice, humanChoice)
+{
+    if (humanChoice === -1)
     {
-        if (humanChoice === -1)
-        {
-            console.log("Input not valid, please enter a one of the three options")
-            return
-        }else if (humanChoice === computerChoice)
-        {
-            console.log("It's a draw!!")
-        }else if (humanChoice === 0 && computerChoice === 2 || 
-                humanChoice === 1 && computerChoice === 0 ||
-                humanChoice === 2 && computerChoice === 1) 
-                {
-                    console.log("You win!")
-                    humanScore ++;    
-                }
-        else
-        {
-            console.log("Computer wins!")
-            computerScore ++;
-        }        
-    }
-
-    while (humanScore<= 5 || computerScore <=5)
+        console.log("Input not valid, please enter a one of the three options")
+        return
+    }else if (humanChoice === computerChoice)
     {
-        
-    }
- }      
+        console.log("It's a draw!!")
+    }else if (humanChoice === 0 && computerChoice === 2 || 
+            humanChoice === 1 && computerChoice === 0 ||
+            humanChoice === 2 && computerChoice === 1) 
+            {
+                console.log("You win!")
+                humanScore ++;    
+            }
+    else
+    {
+        console.log("Computer wins!")
+        computerScore ++;
+    }        
+}
+    /**
+     * Assynchronous function that makes tha game loops 5 times
+     */
+    async function gameLoop()
+    {
+        while (roundNumber <=5)
+        {
+            let computerChoice = getComputerChoice();
+            let humanChoice = getHumanChoice();
 
+            playRound(computerChoice, humanChoice);
+
+            console.log("You have won " + humanScore + " times!!" )
+            console.log("Computer have won " + computerScore + " times!!" )
+            if (roundNumber === 5)
+            {
+                console.log("Game is over!")
+            }
+            await waitTwoSeconds(); // makes the lopp stop for two seconds before next execution
+            roundNumber ++;
+        }
+    }
+    gameLoop() // call game loop function
+}      
+
+// call main function
+playGame();
       
 
-//     WHILE humanScore <= 5 OR computerScore <=5:
-//         SET computerChoice to the return value of getComputerChoice
-//         SET humanChoice to the return value of getHumanChoice
-//         CALL playRound(computerChoice, humanChoice)
-//         CALL sleep(2000) function then continue
 
 
